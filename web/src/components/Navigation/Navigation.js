c19a0726d52579a96ddb11b4406d369e67b29fc1
fMonocle <federico.chahian@gmail.com>
2021-07-04 15:24:34 -0300

    mas test

---
+++ web/src/components/Navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <h1>Aguante Cristina</h1>
+  );
+}
+
+export default Navigation;
